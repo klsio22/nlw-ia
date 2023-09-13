f8dc18412158bbbde3a8b598567655651459eff3
Klesio dev <klesio.dev@gmail.com>
2023-09-12 21:41:23 -0300

    feat(project): added configs initial

---
+++ web-nextjs/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      Ola mundo!
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </main>
+  );
+}
