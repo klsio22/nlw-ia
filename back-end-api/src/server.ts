4a9c76ab431382e9bd4e7980cc54cb6d13da0a42
Klesio dev <klesio.dev@gmail.com>
2023-09-16 01:02:53 -0300

    feat(api): started back end application

---
+++ back-end-api/src/server.ts
@@ -0,0 +1,18 @@
+import { fastify } from "fastify";
+import { prisma } from "./lib/prisma";
+
+const app = fastify();
+
+app.get("/prompts", async () => {
+  const prompts = await prisma.prompt.findMany();
+
+  return prompts;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running!");
+  });
